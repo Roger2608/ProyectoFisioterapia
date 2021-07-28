74d88048b36b4e0ca4a359a4eedf0ca472024e97
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2021-07-28 00:35:52 -0500

    agregando nueva estructura para html

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,10 +1,26 @@
-
 addEventListener('DOMContentLoaded', () => {
-        const boton = document.querySelector('.boton')
-        if (boton) {
-            boton.addEventListener('click', () => {
-                const menu = document.querySelector('.enlaces')
-                menu.classList.toggle('show')
-            })
-        }
+    const boton = document.querySelector('.boton')
+    if (boton) {
+        boton.addEventListener('click', () => {
+            const menu = document.querySelector('.enlaces')
+            menu.classList.toggle('show')
+        })
+    }
 })
+addEventListener('load', () => {
+    UtilitiesJS.consultarArchivosJson('GET', '../JSON/menu.json', true, navbarJS.llenarNavbar);
+})
+
+const navbarJS = {
+    llenarNavbar: (dataNavbar) => {
+        if (dataNavbar != null && dataNavbar != undefined) {
+            const menu = document.querySelector('ul.enlaces');
+            const logo = document.querySelector('div.logo');
+            menu.innerHTML = '';
+            logo.innerHTML = `<img src="/Imagenes/${dataNavbar.logo.imageLogo}" alt="${dataNavbar.logo.nombreLogo}">`;
+            dataNavbar.allCategories.forEach(element => {
+                menu.innerHTML += `<li><a href="${element.redireccion}">${element.nombreCategoria}</a></li>`;
+            });
+        }
+    }
+}
