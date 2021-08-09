550ecfb757bf29f2b3ff60de9ff0271fa655f5e3
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2021-08-08 22:03:57 -0500

    new structure back and front

---
+++ Back-End/Fisioterapia/src/main/java/com/fisioterapia/FisioterapiaApplication.java
@@ -0,0 +1,13 @@
+package com.fisioterapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisioterapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisioterapiaApplication.class, args);
+	}
+
+}
---
+++ Back-End/Fisioterapia/src/test/java/com/fisioterapia/FisioterapiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisioterapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioterapiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
